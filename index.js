b6cde33d8c8f3cfff8f4c7b2598b956582e978d9
Kelly Selden <kellyselden@gmail.com>
2017-02-21 07:45:24 -0800

    rollup

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 /* jshint node: true */
 'use strict';
 
+const mergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel')
+const Rollup = require('broccoli-rollup');
+const path = require('path');
+
 module.exports = {
-  name: 'ember-a-frame'
+  name: 'ember-a-frame',
+
+  // postprocessTree(type, tree) {
+  //   if (type === 'all') {
+  //     console.log(path.dirname(require.resolve('aframe')));
+  //     let aframeTree = new Rollup(this.treeGenerator(path.dirname(require.resolve('aframe'))), {
+  //       inputFiles: '**/*.js',
+  //       rollup: {
+  //         entry: 'index.js',
+  //         dest: 'aframe.js'
+  //       }
+  //     });
+  //     return mergeTrees([aframeTree, tree], { overwrite: true });
+  //   }
+
+  //   return tree;
+  // }
 };
